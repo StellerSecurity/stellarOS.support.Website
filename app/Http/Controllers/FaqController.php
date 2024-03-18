2aac69340f9d1316d510eb826880b9a3839b992f
Your Name <you@example.com>
2024-03-18 14:33:40 +0100

    ..

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -25,6 +25,11 @@ class FaqController extends Controller
                 'id' => 2,
                 'title' => 'How to install apps',
                 'description' => 'Use Aptoide or fdroid to install apps.'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Why does my phone auto reboot?',
+                'description' => 'When your phone havent been unlocked for a certain time, StellarOS will automatically do a lean reboot. This sets your phone in a state called BFU-mode.<br>This makes your phone more secure against any attack towards your data since all encryption keys, data at rest and so on has been cleared and removed.'
             ]
         ];
 
