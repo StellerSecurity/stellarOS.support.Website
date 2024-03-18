00743e06ac5834ffa8f573f1fb39c3c575d704a9
Your Name <you@example.com>
2024-03-18 14:15:27 +0100

    ..

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -9,9 +9,30 @@ class FaqController extends Controller
     public function home()
     {
 
-        $support = File::get(resource_path('faq.json'));
+        $items = [
+            [
+                'id' => 1,
+                'title' => 'How to install eSim',
+                'description' => 'To install eSim on your phone do the following: <br><br>
+                <li>Open owner-user</li>
+                <li>Then open settings</li>
+                <li>Then enable eSim-support</li>
+                <li>Then open mobile & network, then add new simcard, then scan the QR-code</li><br><br>
 
-        return view('faq');
+                You have now the eSim installed. Remember to enable mobile-data.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'How to install apps',
+                'description' => 'Use Aptoide or fdroid to install apps.'
+            ]
+        ];
+
+      //  $support = File::get(resource_path('faq.json'));
+
+
+
+        return view('faq', ['items' => $items]);
     }
 
 }
